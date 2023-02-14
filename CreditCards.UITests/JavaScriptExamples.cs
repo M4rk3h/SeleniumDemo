86e55517662bfad2a1a1f6fb59c2fa1aeb08f241
Markeh <31129951+M4rk3h@users.noreply.github.com>
2023-02-14 15:52:37 +0000

    Got to ClickOverLay - JS Examples.

---
+++ CreditCards.UITests/JavaScriptExamples.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CreditCards.UITests
+{
+    public class JavaScriptExamples
+    {
+        [Fact]
+        public void ClickOverlayedLink() 
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://localhost:44108/jsoverlay.html");
+
+                DemoHelper.Pause();
+
+                driver.FindElement(By.Id("HiddenLink")).Click();
+
+                Assert.Equal("https://pluralsight.com/", driver.Url);
+            }
+        }
+    }
+}
