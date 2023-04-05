5f070aff5379da317203236c13a1440f120a3bbb
Markeh <31129951+M4rk3h@users.noreply.github.com>
2023-04-05 11:23:03 +0100

    More Refactoring.
    Adding a re-usable chrome webdriver.

--- CreditCards.UITests/_03CreditCardApplicationShould.cs
+++ CreditCards.UITests/_03CreditCardApplicationShould.cs
@@ -1,11 +1,8 @@
 ﻿using CreditCards.UITests.PageObjectModels;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.DevTools.V85.ApplicationCache;
-using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 using System;
-using System.Data.SqlClient;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -13,117 +10,100 @@ namespace CreditCards.UITests
 {
     // Add Test
     [Trait("Category", "Applications")]
-    public class _03CreditCardApplicationShould
+    public class _03CreditCardApplicationShould : IClassFixture<ChromeDriverFixture>
     {
         private const string HomeUrl = "http://localhost:44108/";
         private const string ApplyUrl = "http://localhost:44108/Apply";
+        private readonly ChromeDriverFixture ChromeDriverFixture;
 
-        private readonly ITestOutputHelper output;
-
-        public _03CreditCardApplicationShould(ITestOutputHelper output)
+        public _03CreditCardApplicationShould(ChromeDriverFixture chromeDriverFixture)
         {
-            this.output = output;
+            ChromeDriverFixture = chromeDriverFixture;
+            ChromeDriverFixture.Driver.Manage().Cookies.DeleteAllCookies();
+            ChromeDriverFixture.Driver.Navigate().GoToUrl("about:blank");
+
         }
 
         [Fact]
         public void _1BeInitiatedFromHomePage_NewLowRate()
         {
-            using (IWebDriver driver = new ChromeDriver())
-            {
-                var homePage = new HomePage(driver);
-                homePage.NavigateTo();
-                ApplicationPage applicationPage = homePage.ClickApplyLowRateLink();
-                applicationPage.EnsurePageLoaded();
-            }
+            var homePage = new HomePage(ChromeDriverFixture.Driver);
+            homePage.NavigateTo();
+            ApplicationPage applicationPage = homePage.ClickApplyLowRateLink();
+            applicationPage.EnsurePageLoaded();
+
         }
 
         [Fact]
         public void _2BeInitiatedFromHomePage_EasyApplication()
         {
-            using (IWebDriver driver = new ChromeDriver())
-            {
-                var homePage = new HomePage(driver);
-                homePage.NavigateTo();
-                homePage.WaitForEasyApplicationCarouselPage();
-                ApplicationPage applicationPage = homePage.ClickApplyEasyApplicationLink();
-                applicationPage.EnsurePageLoaded();
-            }
+            var homePage = new HomePage(ChromeDriverFixture.Driver);
+            homePage.NavigateTo();
+            homePage.WaitForEasyApplicationCarouselPage();
+            ApplicationPage applicationPage = homePage.ClickApplyEasyApplicationLink();
+            applicationPage.EnsurePageLoaded();
         }
 
         [Fact]
         public void _3BeInitiatedFromHomePage_CustomerService()
         {
-            using (IWebDriver driver = new ChromeDriver())
-            {
-                var homePage = new HomePage(driver);
-                homePage.NavigateTo();
-                output.WriteLine($"{DateTime.Now.ToLongTimeString()} Navigating to '{HomeUrl}'");
-                output.WriteLine($"{DateTime.Now.ToLongTimeString()} Finding element using explicity");
-                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(35));
-                // Use built in function instead of bespoke (above)
-                IWebElement applyLink = wait.Until(ExpectedConditions.ElementToBeClickable(By.ClassName("customer-service-apply-now")));
-                output.WriteLine($"{DateTime.Now.ToLongTimeString()} Found element Displayed={applyLink.Displayed} Enabled={applyLink.Enabled}");
-                // Click on the button
-                applyLink.Click();
-                output.WriteLine($"{DateTime.Now.ToLongTimeString()} Clicking element");
-                //homePage.EnsurePageLoaded();
-                output.WriteLine($"Page URL: {driver.Url}");
-                
-            }
+            var homePage = new HomePage(ChromeDriverFixture.Driver);
+            homePage.NavigateTo();
+            
+            WebDriverWait wait = new WebDriverWait(ChromeDriverFixture.Driver, TimeSpan.FromSeconds(35));
+            // Use built in function instead of bespoke (above)
+            IWebElement applyLink = wait.Until(ExpectedConditions.ElementToBeClickable(By.ClassName("customer-service-apply-now")));
+            
+            // Click on the button
+            applyLink.Click();
         }
 
         [Fact]
         public void _4BeInitiatedFromHomePage_RandomGreeting()
         {
-            using (IWebDriver driver = new ChromeDriver())
-            {
-                var homePage = new HomePage(driver);
-                homePage.NavigateTo();
+            var homePage = new HomePage(ChromeDriverFixture.Driver);
+            homePage.NavigateTo();
 
-                IWebElement randomPartial = driver.FindElement(By.PartialLinkText("- Apply Now!"));
-                randomPartial.Click();
-                // Wait 1 second
-                DemoHelper.Pause();
+            IWebElement randomPartial = ChromeDriverFixture.Driver.FindElement(By.PartialLinkText("- Apply Now!"));
+            randomPartial.Click();
+            // Wait 1 second
+            DemoHelper.Pause();
 
-                homePage.EnsurePageLoaded();
-            }
+            var appPage = new ApplicationPage(ChromeDriverFixture.Driver);
+            appPage.EnsurePageLoaded();
         }
 
         [Fact]
         public void _5BeInitiatedFromHomePage_RandomGreeting_XPath()
         {
-            using (IWebDriver driver = new ChromeDriver())
-            {
-                var homePage = new HomePage(driver);
-                homePage.NavigateTo();
+            var homePage = new HomePage(ChromeDriverFixture.Driver);
+            homePage.NavigateTo();
 
-                IWebElement randomPartial = driver.FindElement(By.XPath("//a[text()[contains(.,'- Apply Now!')]]"));
-                randomPartial.Click();
-                // Wait 1 second
-                //DemoHelper.Pause();
+            IWebElement randomPartial = ChromeDriverFixture.Driver.FindElement(By.XPath("//a[text()[contains(.,'- Apply Now!')]]"));
+            randomPartial.Click();
+            // Wait 1 second
+            //DemoHelper.Pause();
 
-                homePage.EnsurePageLoaded();
-            }
+            var appPage = new ApplicationPage(ChromeDriverFixture.Driver);
+            appPage.EnsurePageLoaded();
         }
 
         [Fact]
         public void _6BIFHP_EA_PC()
         {
-            using (IWebDriver driver = new ChromeDriver())
-            {
-                var homePage = new HomePage(driver);
-                homePage.NavigateTo();
+            var homePage = new HomePage(ChromeDriverFixture.Driver);
+            homePage.NavigateTo();
+            DemoHelper.Pause();
+            WebDriverWait wait = new WebDriverWait(ChromeDriverFixture.Driver, TimeSpan.FromSeconds(11));
 
-                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(11));
+            // The below method is depreciated - source code copied into ExpectedConditions.cs
+            // from https://github.com/DotNetSeleniumTools/DotNetSeleniumExtras/blob/master/src/WaitHelpers/ExpectedConditions.cs
+            IWebElement applyLink = wait.Until(ExpectedConditions.ElementToBeClickable(By.LinkText("Easy: Apply Now!")));
+            applyLink.Click();
+            DemoHelper.Pause();
 
-                // The below method is depreciated - source code copied into ExpectedConditions.cs
-                // from https://github.com/DotNetSeleniumTools/DotNetSeleniumExtras/blob/master/src/WaitHelpers/ExpectedConditions.cs
-                IWebElement applyLink = wait.Until(ExpectedConditions.ElementToBeClickable(By.LinkText("Easy: Apply Now!")));
-
-                applyLink.Click();
-
-                homePage.EnsurePageLoaded();
-            }
+            var appPage = new ApplicationPage(ChromeDriverFixture.Driver);
+            appPage.EnsurePageLoaded();
         }
 
         [Fact]
@@ -135,32 +115,29 @@ namespace CreditCards.UITests
             const string Age = "30";
             const string Income = "50000";
 
-            using (IWebDriver driver = new ChromeDriver())
-            {
-                var applicationPage = new ApplicationPage(driver);
-                applicationPage.NavigateTo();
+            var applicationPage = new ApplicationPage(ChromeDriverFixture.Driver);
+            applicationPage.NavigateTo();
 
-                applicationPage.EnterFirstName(FirstName);
-                applicationPage.EnterLastName(LastName);
-                applicationPage.EnterFrequentFlyerNumber(Number);
-                applicationPage.EnterAge(Age);
-                applicationPage.EnterGrossAnnualIncome(Income);
-                applicationPage.ChooseMaritalStatusMarried();
-                applicationPage.ChooseBusinessSourceTV();
-                applicationPage.AcceptTerms();
-                ApplicationCompletePage applicationCompletePage = applicationPage.SubmitApplication();
-                DemoHelper.Pause();
+            applicationPage.EnterFirstName(FirstName);
+            applicationPage.EnterLastName(LastName);
+            applicationPage.EnterFrequentFlyerNumber(Number);
+            applicationPage.EnterAge(Age);
+            applicationPage.EnterGrossAnnualIncome(Income);
+            applicationPage.ChooseMaritalStatusMarried();
+            applicationPage.ChooseBusinessSourceTV();
+            applicationPage.AcceptTerms();
+            ApplicationCompletePage applicationCompletePage = applicationPage.SubmitApplication();
+            DemoHelper.Pause();
 
-                applicationCompletePage.EnsurePageLoaded();
+            applicationCompletePage.EnsurePageLoaded();
 
-                Assert.Equal("ReferredToHuman", applicationCompletePage.Decision);
-                Assert.NotEmpty(applicationCompletePage.ReferenceNumber);
-                Assert.Equal($"{FirstName} {LastName}", applicationCompletePage.FullName);
-                Assert.Equal(Age, applicationCompletePage.Age);
-                Assert.Equal(Income, applicationCompletePage.Income);
-                Assert.Equal("Married", applicationCompletePage.RelationshipStatus);
-                Assert.Equal("TV", applicationCompletePage.BusinessSource);
-            }
+            Assert.Equal("ReferredToHuman", applicationCompletePage.Decision);
+            Assert.NotEmpty(applicationCompletePage.ReferenceNumber);
+            Assert.Equal($"{FirstName} {LastName}", applicationCompletePage.FullName);
+            Assert.Equal(Age, applicationCompletePage.Age);
+            Assert.Equal(Income, applicationCompletePage.Income);
+            Assert.Equal("Married", applicationCompletePage.RelationshipStatus);
+            Assert.Equal("TV", applicationCompletePage.BusinessSource);
         }
 
         [Fact]
@@ -170,37 +147,33 @@ namespace CreditCards.UITests
             const string firstName = "Mark";
             const string invalidAge = "17";
             const string validAge = "30";
-
-            using (IWebDriver driver = new ChromeDriver())
-            {
-                // Get and Set ApplicationPage
-                // Get Driver from ApplicationPage and go to correct URL.
-                var applicationPage = new ApplicationPage(driver);
-                applicationPage.NavigateTo();
-                // Fill out the form
-                applicationPage.EnterFirstName(firstName);
-                // Don't enter last name
-                applicationPage.EnterFrequentFlyerNumber("01633");
-                applicationPage.EnterAge(invalidAge);
-                applicationPage.EnterGrossAnnualIncome("50000");
-                applicationPage.ChooseMaritalStatusMarried();
-                applicationPage.ChooseBusinessSourceTV();
-                applicationPage.AcceptTerms();
-                applicationPage.SubmitApplication();
-                // Assert validation failed
-                Assert.Equal(2, applicationPage.ValidationErrorMessages.Count);
-                Assert.Contains("Please provide a last name", applicationPage.ValidationErrorMessages);
-                Assert.Contains("You must be at least 18 years old", applicationPage.ValidationErrorMessages);
-                // Fix validation errors
-                applicationPage.EnterLastName("Baber");
-                applicationPage.ClearAge();
-                applicationPage.EnterAge(validAge);
-                // Resubmit form
-                ApplicationCompletePage applicationCompletePage = applicationPage.SubmitApplication();
-                // Check form submitted
-                applicationCompletePage.EnsurePageLoaded();
-
-            }
+            
+            // Get and Set ApplicationPage
+            // Get Driver from ApplicationPage and go to correct URL.
+            var applicationPage = new ApplicationPage(ChromeDriverFixture.Driver);
+            applicationPage.NavigateTo();
+            // Fill out the form
+            applicationPage.EnterFirstName(firstName);
+            // Don't enter last name
+            applicationPage.EnterFrequentFlyerNumber("01633");
+            applicationPage.EnterAge(invalidAge);
+            applicationPage.EnterGrossAnnualIncome("50000");
+            applicationPage.ChooseMaritalStatusMarried();
+            applicationPage.ChooseBusinessSourceTV();
+            applicationPage.AcceptTerms();
+            applicationPage.SubmitApplication();
+            // Assert validation failed
+            Assert.Equal(2, applicationPage.ValidationErrorMessages.Count);
+            Assert.Contains("Please provide a last name", applicationPage.ValidationErrorMessages);
+            Assert.Contains("You must be at least 18 years old", applicationPage.ValidationErrorMessages);
+            // Fix validation errors
+            applicationPage.EnterLastName("Baber");
+            applicationPage.ClearAge();
+            applicationPage.EnterAge(validAge);
+            // Resubmit form
+            ApplicationCompletePage applicationCompletePage = applicationPage.SubmitApplication();
+            // Check form submitted
+            applicationCompletePage.EnsurePageLoaded();
         }
     }
 }
